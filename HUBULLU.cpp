fc477de249a61f6123bfd38488b109add95264fd
shubhamkr2020 <shubhamkr2020@gmail.com>
2017-07-03 14:23:08 +0530

    Create HUBULLU.cpp

---
+++ HUBULLU.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,j,k) for(i=j; i<k; i++)
+#define rrep(i,j,k) for(i=j; i>=k; i--)
+#define ZERO(a) memset(a,0,sizeof(a))
+#define mem(a,b) memset(a,b,sizeof(a))
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define lowbit(x) (x&(-x))
+#define it(x) x::iterator it
+#define gc getchar_unlocked
+inline int sint()
+   {int NR=0,sign=1;    char c=gc();
+    while( c < 48 || c > 57 ){if(c=='-')sign=0; c=gc();}
+    while(c>47 && c< 58)
+    {    NR = (NR << 3) + (NR << 1) + (c - 48);    c=gc();    }
+    return (sign?NR:(-NR));
+}
+
+ typedef long long ll;
+ typedef vector<int> vi;
+ typedef vector<string> vs;
+ typedef vector<vi> vvi;
+ typedef pair<string,int> psi;
+ typedef pair<int,string> pis;
+ typedef pair<int,int> pii;
+
+int main() {
+	
+	
+	int t;
+	cin>>t;
+	while(t--)
+    {
+        int n,f;
+        cin>>n>>f;
+        string fir = "Airborne wins.",sec = "Pagfloyd wins.";
+        if(f)
+        {
+            fir = "Pagfloyd wins.";
+            sec = "Airborne wins.";
+        }
+        
+        cout<<fir<<endl;
+        
+        
+    }
+	return 0;
+}
