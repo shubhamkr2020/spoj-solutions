b680dc2d2909f3035c521b4c3497b1337a6f63c6
shubhamkr2020 <shubhamkr2020@gmail.com>
2017-12-30 15:59:13 +0530

    Create CPRMT.cpp

---
+++ CPRMT.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,j,k) for(i=j; i<k; i++)
+#define rrep(i,j,k) for(i=j; i>=k; i--)
+#define tr(c,it) for(auto it = (c).begin(); it != (c).end(); it++)
+#define ZERO(a) memset(a,0,sizeof(a))
+#define lastbit(x) (x&(-x)) 
+#define pb push_back 
+#define all(c) (c).begin(),(c).end()
+#define fin scan_int()
+#define pd(x) printf("%d",x)
+#define newl printf("\n")
+#define xx first
+#define yy second
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define compress(s) sort(all(s)); s.resize(unique(s.begin(),s.end())-s.begin());
+#define strtoint(s,n) if (!(istringstream(s) >> n) ) n = 0;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
+typedef pair<string,int> psi; 
+typedef pair<int,int> pii;
+
+string inttostr(int str){stringstream stream;stream << str;return stream.str();}
+ll modexpo(ll a,ll b,ll n){ll d=1;while(b){if(b&1)d=(d*a)%n;b>>=1;a=(a*a)%n;}return d;}
+ll expo(ll a,ll b){ll d=1;while(b){if(b&1)d=d*a;b>>1;a=a*a;}return d;}
+inline ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
+inline ll lcm(ll a,ll b) {return (a*b)/gcd(a,b);}
+ll inv(ll a,ll m){return modexpo(a,m-2,m);}
+
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////
+#define gc getchar_unlocked
+inline int scan_int() {
+   int NR=0,sign=1;    char c=gc();
+    while( c < 48 || c > 57 ){if(c=='-')sign=0; c=gc();}
+    while(c>47 && c< 58)
+    {    NR = (NR << 3) + (NR << 1) + (c - 48);    c=gc();    }
+    return (sign?NR:(-NR));}
+inline void fast_out(int x){
+         register char buffor[35]; register int i=0;
+         do{buffor[i++]=(x%10)+'0';
+               x/=10;
+            } while(x); i--;
+            while(i>=0) putchar(buffor[i--]);
+            putchar('\n');
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+
+
+int main(){
+    FAST
+    // freopen("input.in","r",stdin);
+    // freopen("output.out","w",stdout);
+    
+    do {
+        string a,b;
+        cin>>a>>b;
+        if(a=="" && b=="")
+        break;
+        int h[256] = {0},g[256] = {0};
+        int i;
+        rep(i,0,a.length()) h[a[i]]++;
+        rep(i,0,b.length()) g[b[i]]++;
+        
+        string s;
+        rep(i,0,257) {
+            int j;
+            rep(j,0,min(h[i],g[i]))
+            s += char(i);
+        }
+        cout<<s<<endl;
+    }while(1);
+    
+    return 0;
+}
