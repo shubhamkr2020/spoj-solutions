4b6a98079997ff018712b3c485575e6f325b6611
shubhamkr2020 <shubhamkr2020@gmail.com>
2017-12-30 15:59:39 +0530

    Create CRDS.cpp

---
+++ CRDS.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,j,k) for(i=j; i<k; i++)
+#define rrep(i,j,k) for(i=j; i>=k; i--)
+#define ZERO(a) memset(a,0,sizeof(a))
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define lowbit(x) (x&(-x))
+#define it(x) x::iterator it
+#define gc getchar_unlocked
+inline int sint()
+   {int NR=0,sign=1;    char c=gc();
+    while( c < 48 || c > 57 ){if(c=='-')sign=0; c=gc();}
+    while(c>47 && c< 58)
+    {    NR = (NR << 3) + (NR << 1) + (c - 48);    c=gc();    }
+    return (sign?NR:(-NR));
+}
+
+ typedef long long ll;
+ typedef vector<int> vi;
+ typedef vector<string> vs;
+ typedef vector<vi> vvi;
+ typedef pair<string,int> psi;
+ typedef pair<int,string> pis;
+ typedef pair<int,int> pii;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    ll n;
+	    cin>>n;
+	    ll ans = (3*n*(n+1))/2 - n;
+	    ans = ans%1000007;
+	    cout<<ans%1000007<<endl;
+	}
+	
+	return 0;
+}
